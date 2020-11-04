1e431dce6682137e1b609a0ec4984e4a65a3bd99
Scott Davies <scottdavies1983@gmail.com>
2020-11-04 19:35:24 +0000

    getting started with state

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,22 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to our fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes objevct to state
+    this.setState({
+      fishes,
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +28,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
